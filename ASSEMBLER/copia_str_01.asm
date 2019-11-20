bb08a5a82445ddee26e7b2d431552cc4590ed200
pablitocalvo <pablitocalvo@users.noreply.github.com>
2019-11-20 18:11:22 +0100

    Create copia_str_01.asm

---
+++ ASSEMBLER/copia_str_01.asm
@@ -0,0 +1,23 @@
+; questa riga è un commento inizia con il carattere punto e virgola
+
+        mov a, stringa   
+        mov b, destinazione 
+ciclo:	mov  c,[a]
+        mov [b],c
+	
+        cmp c,0
+	      jz fineciclo
+
+	      inc a
+        inc b
+
+	      jmp ciclo
+
+fineciclo:
+	      hlt
+
+stringa:  DB "PABLITOCALVO IS THE BEST!"
+          DB 0
+
+
+destinazione: 
