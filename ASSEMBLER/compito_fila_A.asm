26bedba7305d357e59e8c3e43098da1dc661d3e2
pablitocalvo <pablitocalvo@users.noreply.github.com>
2019-12-12 11:19:52 +0100

    Create compito_fila_A.asm

---
+++ ASSEMBLER/compito_fila_A.asm
@@ -0,0 +1,21 @@
+; fila A
+
+  MOV  A,0x60 
+  MOV  D,0x70 
+  MOV  B, 10 
+  MOV  C, 4 
+ciclo: MOV  [A], B 
+  INC  A 
+  MOV  [D], 1 
+  INC  D 
+  DEC  B 
+  DEC  C 
+  JNZ  ciclo 
+  INC  B 
+  MOV  C, 4 
+ciclo2: MOV  [A], B 
+  INC  A 
+  INC  B 
+  DEC  C 
+  JNZ  ciclo2 
+  HLT 
